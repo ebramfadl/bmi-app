656a88ad8bb2a942a32660f0d1ae7d93ba6e5596
ebramfadl <ebram@ntsal.com>
2023-09-26 17:23:34 +0300

    adjusting the vertical values

--- lib/screens/chart.dart
+++ lib/screens/chart.dart
@@ -16,18 +16,25 @@ class Chart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<ChartData> chartData = [
-      ChartData("S", 100),
-      ChartData("S", 200),
-      ChartData("M", 300),
-      ChartData("T", 400),
-      ChartData("W", 500),
-      ChartData("T", 600),
-      ChartData("F", 700),
+      ChartData("Sat", 100),
+      ChartData("Sun", 200),
+      ChartData("Mon", 300),
+      ChartData("Tue", 400),
+      ChartData("Wed", 500),
+      ChartData("Thu", 600),
+      ChartData("Fri", 700),
     ];
+
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
+        body: Center(child: LayoutBuilder(builder: (context, constraints) {
+          double screenHeight = MediaQuery.of(context).size.height;
+          double containerHeight = screenHeight * 0.5;
+          return Center(
             child: Container(
+              height: containerHeight,
+              child: Directionality(
+                textDirection: TextDirection.rtl,
                 child: SfCartesianChart(
                     primaryXAxis: CategoryAxis(
                       title: AxisTitle(text: 'X-Axis'),
@@ -36,26 +43,34 @@ class Chart extends StatelessWidget {
                         color: Colors.black, // Change the font color here
                       ),
                       majorGridLines: MajorGridLines(width: 0),
-                      labelPlacement: LabelPlacement.betweenTicks,
+                      labelPlacement: LabelPlacement.onTicks,
+                      edgeLabelPlacement: EdgeLabelPlacement.shift,
                     ),
 
                     // Set the primaryYAxis to customize the appearance of the y-axis labels.
-                    primaryYAxis: NumericAxis(
-                      title: AxisTitle(text: 'Y-Axis'),
-                      // Configure the text style for y-axis labels.
-                      labelStyle: TextStyle(
-                        color: Colors.black, // Change the font color here
-                      ),
-                    ),
+                    // primaryYAxis: NumericAxis(
+                    //   title: AxisTitle(text: 'Y-Axis'),
+                    //   // Configure the text style for y-axis labels.
+                    //   labelStyle: TextStyle(
+                    //     color: Colors.black, // Change the font color here
+                    //   ),
+                    //   labelPosition: ChartDataLabelPosition.inside,
+                    // ),
                     series: <ChartSeries<ChartData, String>>[
-              ColumnSeries<ChartData, String>(
-                  dataSource: chartData,
-                  xValueMapper: (ChartData data, _) => data.x,
-                  yValueMapper: (ChartData data, _) => data.y,
-                  // Width of the columns
-                  width: 0.2,
-                  // Spacing between the columns
-                  spacing: 0.1)
-            ]))));
+                      ColumnSeries<ChartData, String>(
+                          dataSource: chartData,
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          pointColorMapper: (ChartData data, _) =>
+                              Color(0xFFC2FFE6),
+                          // Width of the columns
+                          width: 0.8,
+                          // Spacing between the columns
+                          spacing: 0.2)
+                    ]),
+              ),
+            ),
+          );
+        })));
   }
 }
